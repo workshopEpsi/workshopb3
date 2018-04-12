71c8fa0bca57ac2944da65738d1635ed1e737b69
Unknown <anthonytrabichet@gmail.com>
2018-04-12 10:46:14 +0200

    fixed router

--- src/AppBundle/Controller/ConnexionUtilisateurController.php
+++ src/AppBundle/Controller/ConnexionUtilisateurController.php
@@ -8,4 +8,53 @@ use Symfony\Component\HttpFoundation\Request;
 class ConnexionUtilisateurController extends Controller
 {
 
+    public function connexionEleveAction(Request $request)
+    {
+        $login = $request->request->get('_email');
+        $mdp = $request->request->get('_password');
+
+        $eleve = $this->getDoctrine()->getRepository('AppBundle:Etudiants')->findBy(['email' => $login]);
+
+        $reponse;
+
+        if(!$eleve) {
+            $reponse = $this->render('connexion.html.twig', array('erreur' => true));
+        }
+
+
+        if($eleve->getMdp() == $mdp) {
+            $reponse = $this->render('Etudiant/EspaceEtudiant.html.twig', array('login' => $login) );
+        } else {
+            $reponse = $this->render('connexion.html.twig', array('erreur' => true) );
+        }
+
+        return $reponse;
+    }
+
+
+
+
+    public function connexionInterPedaAction(Request $request)
+    {
+        $login = $request->request->get('_email');
+        $mdp = $request->request->get('_password');
+
+        $intervenant = $this->getDoctrine()->getRepository('AppBundle:Intervenants')->findBy(['email' => $login]);
+
+        $reponse;
+
+        if(!$intervenant) {
+            $reponse = $this->render('connexion.html.twig', array('erreur' => true));
+        }
+
+
+        if($eleve->getMdp() == $mdp) {
+            $reponse = $this->render('Intervenant/EspaceIntervenant.html.twig', array('login' => $login) );
+        } else {
+            $reponse = $this->render('connexion.html.twig', array('erreur' => true) );
+        }
+
+        return $reponse;
+
+    }
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,9 +11,7 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+
     public function indexAction(Request $request, SessionInterface $session)
     {
 //        if connected
@@ -23,63 +21,10 @@ class DefaultController extends Controller
 //                return $this->render('Intervenant/EspaceIntervenant.html.twig');
 //          else
 //            return $this->render('connexion.html.twig');
-            return $this->render('connexion.html.twig');
-    }
-
-
-    /**
-     * @Route("/logineleve", name="homepage")
-     */
-    public function connexionEleveAction(Request $request)
-    {
-        $login = $request->request->get('_email');
-        $mdp = $request->request->get('_password');
-
-        $eleve = $this->getDoctrine()->getRepository('AppBundle:Etudiants')->findBy(['email' => $login]);
-
-        $reponse;
-
-        if(!$eleve) {
-            $reponse = $this->render('connexion.html.twig', array('erreur' => true));
-        }
-
-
-        if($eleve->getMdp() == $mdp) {
-            $reponse = $this->render('Etudiant/EspaceEtudiant.html.twig', array('login' => $login) );
-        } else {
-            $reponse = $this->render('connexion.html.twig', array('erreur' => true) );
-        }
-
-        return $reponse;
+            return $this->render('index.html.twig');
     }
 
 
 
-    /**
-     * @Route("/loginpeda", name="homepage")
-     */
-    public function connexionInterPedaAction(Request $request)
-    {
-        $login = $request->request->get('_email');
-        $mdp = $request->request->get('_password');
-
-        $intervenant = $this->getDoctrine()->getRepository('AppBundle:Intervenants')->findBy(['email' => $login]);
-
-        $reponse;
-
-        if(!$intervenant) {
-            $reponse = $this->render('connexion.html.twig', array('erreur' => true));
-        }
-
-
-        if($eleve->getMdp() == $mdp) {
-            $reponse = $this->render('Intervenant/EspaceIntervenant.html.twig', array('login' => $login) );
-        } else {
-            $reponse = $this->render('connexion.html.twig', array('erreur' => true) );
-        }
-
-        return $reponse;
-
-    }
 
 }
